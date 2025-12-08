3fd7918e7f2e715426fe921d3111f5f9f8994dff
username-xboybx <jeswanth012@gmail.com>
2025-12-08 13:13:27 +0530

    added new throbber loader

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../index.css";
 
 const LoadingSpinner = () => {
   return (
-    <div className="font-4xl bg-gradient-to-r from-black via-white to-black bg-[200%] bg-[length:200%_200%] bg-clip-text text-transparent [text-fill-color:transparent] animate-gradient-loader [-webkit-text-fill-color:transparent] [-webkit-background-clip:text]">
-      3 2 1 Beatbox...
+    <div className="flex items-center">
+      <div className="font-4xl bg-gradient-to-r from-black via-white to-black bg-[200%] bg-[length:200%_200%] bg-clip-text text-transparent [text-fill-color:transparent] animate-gradient-loader [-webkit-text-fill-color:transparent] [-webkit-background-clip:text]">
+        3 2 1 Beatbox...
+      </div>
+      <div className="ml-3">
+        <div class="inline-block w-3 h-3 rounded-full border border-gray-300 border-t-transparent animate-spin"></div>
+      </div>
     </div>
   );
 };
